8190ab3b2b70993c0b981511091b3c3a532377cd
jameswu05 <145163478+jameswu05@users.noreply.github.com>
2024-03-12 17:18:52 -0400

    started algorithm to get count, size, and size type(units)
    function will be modified so that it uses .find to reduce runtime

--- parse.py
+++ parse.py
@@ -1,15 +1,56 @@
 import json
 file_path = './json/cvs/apple.json'
 
+def getCountAndSize(string):
+  ret = dict()
+  for i in reversed(range(len(string))):
+    if string[i].lower() == "t":
+      i += 1
+      if string[i:i+2].lower() == "c":
+        count = ""
+        i -= 2
+        while string[i].isdigit() or string[i] == " ":
+          count += string[i]
+          i -= 1
+        ret['Count'] = count
+
+        # modify this function so that it uses .find instead of for loops
+
+
 def parseCVS(data):
-  if '-' in data[0]:
-    elem1 = (data[0].split(' - '))
+  if ',' in data[0]:
+    elem1 = (data[0].split(', '))
     name = elem1[0]
+
     for i in range(1, len(elem1)):
       if elem1[i][0].isdigit():
         quantity = elem1[i]
+
+        for i in range(len(quantity)):
+          if quantity[i].isalpha():
+            quantity_type = quantity[i:]
+
     print("Name: ", name)
     print("Quantity: ", quantity)
+    print("Quantity Type: ", quantity_type)
+
+  elif "-" in data[0]:
+    elem1 = (data[0].split(' = '))
+    name = elem1[0]
+
+    for i in range(1, len(elem1)):
+      if elem1[i][0].isdigit():
+        quantity = elem1[i]
+
+        for i in range(len(quantity)):
+          if quantity[i].isalpha():
+            quantity_type = quantity[i:]
+
+
+    print("Name: ", name)
+    print("Quantity: ", quantity)
+    print("Quantity Type: ", quantity_type)
+
   else:
     elem1 = data[0]
     name = elem1
