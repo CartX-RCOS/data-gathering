7cd48a5eeb17f51adc57ccd3d8c428343aee3f88
jameswu05 <145163478+jameswu05@users.noreply.github.com>
2024-03-17 15:28:31 -0400

    wrote algorithm to parse cvs json file (name, size, size type)
    some categories do not list size or size type

---
+++ parseCVS.py
@@ -0,0 +1,32 @@
+import json
+
+def parseCVS(data):
+    ret = dict()
+    name_list = data["name"].split("-")
+    name = ""
+    j = 0
+    for i in range(0, len(name_list)):
+        if name_list[i].isdigit():
+            j += 1
+            break
+        name += name_list[i] + " "
+        j += 1
+    ret["Name"] = name.strip()
+    nums = name_list[j:]
+    if nums:
+        if len(nums) >= 3:
+            size = nums[1]
+            ret["Size"] = size
+        else:
+            size = nums[0]
+            ret["Size"] = size
+        size_type = nums[-1]
+        ret["Size Type"] = size_type
+    return ret
+
+with open('cvs.json', 'r') as file:
+    data = json.load(file)
+
+for item in data:
+    ret = parseCVS(item)
+    print(ret)
