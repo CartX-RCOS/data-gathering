79c0e47591f7f722c05cec6402d5ceca6d3ca49b
jameswu05 <145163478+jameswu05@users.noreply.github.com>
2024-03-19 16:49:34 -0400

    changed conditionals in function and account for dashes between numbers
    fixed some corner cases

--- parseCVS.py
+++ parseCVS.py
@@ -1,21 +1,25 @@
 import json
 
+all_possible_endings = ["ct", "CT", "oz", "OZ", "mg", "MG"]
+
 def parseCVS(data):
     ret = dict()
     name_list = data["name"].split("-")
     name = ""
+
     j = 0
     for i in range(0, len(name_list)):
-        if name_list[i].isdigit():
-            j += 1
+        if name_list[i].isdigit() and name_list[i+1] not in all_possible_endings:
             break
         name += name_list[i] + " "
         j += 1
+
     ret["Name"] = name.strip()
     nums = name_list[j:]
+    # print(nums)
     if nums:
         if len(nums) >= 3:
-            size = nums[1]
+            size = '.'.join(nums[:2])
             ret["Size"] = size
         else:
             size = nums[0]
