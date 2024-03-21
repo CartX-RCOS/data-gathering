f4abf0b00fe29c08466769592c0c5cd3949f591f
Nikul Patel <pateln14@rpi.edu>
2024-03-21 16:53:50 -0400

    fixed parsing code

--- parseCVS.py
+++ parseCVS.py
@@ -1,31 +1,77 @@
 import json
 
-all_possible_endings = ["ct", "CT", "oz", "OZ", "mg", "MG"]
+all_possible_endings = ["ct", "CT", "oz", "OZ", "mg", "MG", "pack"]
 
-def parseCVS(data):
-    ret = dict()
-    name_list = data["name"].split("-")
-    name = ""
+# checks if there is something wrong
+def contains_substring(main_str, substrings):
+    main_str = main_str.lower()  # Convert to lowercase for case-insensitive comparison
+    for substring in substrings:
+        if substring.lower() in main_str:
+            return True
+    return False
 
-    j = 0
-    for i in range(0, len(name_list)):
-        if name_list[i].isdigit() and name_list[i+1] not in all_possible_endings:
-            break
-        name += name_list[i] + " "
-        j += 1
+# gets the size
+def get_size_info(elements):
+    # Initialize default values
+    index = -1
+    size = None
+    size_type = None
 
-    ret["Name"] = name.strip()
-    nums = name_list[j:]
-    # print(nums)
-    if nums:
-        if len(nums) >= 3:
-            size = '.'.join(nums[:2])
-            ret["Size"] = size
+    # Check if the last two elements are numbers (considering a decimal size)
+    if len(elements) >= 3 and elements[-3].replace('.', '', 1).isdigit() and elements[-2].isdigit():
+        size = f"{elements[-3]}.{elements[-2]}"  # Format as "X.Y ounces"
+        size_type = elements[-1]
+        index = -3  # Update index if a decimal size is identified
+    elif elements[-2].isdigit():
+        size = f"{elements[-2]}"  # Format as "X ounces" if only one number is present
+        size_type = elements[-1]
+        index = -2  # Update index for a whole number size
+    else:
+        return 0,0,0,0
+    
+    return index, elements[0:(len(elements)+index)], size, size_type
+
+# joins the name of the products
+def join_with_apostrophe(words):
+    # Initialize an empty string to hold the result
+    result = ''
+    # Iterate over the list of words
+    for i in range(len(words)):
+        # If the current word is 's' (and it's not the first word), prepend it with an apostrophe
+        if words[i] == 's' and i > 0:
+            result += "'" + words[i]
         else:
-            size = nums[0]
-            ret["Size"] = size
-        size_type = nums[-1]
-        ret["Size Type"] = size_type
+            # If it's not a special case, just append the word
+            # Add a space before the word if it's not the first word
+            if i > 0:
+                result += ' '
+            result += words[i]
+    return result.capitalize()
+
+# parses the function
+def parseCVS(data):
+    ret = {}
+    name_list = data["name"].split("-")
+    answer = (get_size_info(name_list));
+
+    if (answer[0] != 0):
+
+        # get the name
+        name = answer[1]
+        display = True
+
+        # there is still something wrong
+        for j in range(0,len(answer[1])):
+            if (contains_substring(answer[1][j], all_possible_endings) == True):
+                display = False
+                break
+
+        if (display): 
+            print("name: ", (answer[1]));
+            print("name: ", join_with_apostrophe(answer[1]))
+            print("size: ", answer[2]);
+            print("quanity: ", answer[3]);
+    
     return ret
 
 with open('cvs.json', 'r') as file:
